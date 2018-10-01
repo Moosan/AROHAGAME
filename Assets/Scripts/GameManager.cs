6f16b3562f8359a96bcd8e0cec2ad567918a37b3
Moosan <washer2016@outlook.jp>
2018-10-02 02:56:43 +0900

    addproject

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject player;
+   public Material blue;
+    // Use this for initialization
+    private static int ArohaMax;
+    private static int ArohaCount;
+    private static bool gameStart;
+	void Start () {
+        ArohaMax = 5;
+        ArohaCount = 0;
+        gameStart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ArohaCount==ArohaMax)
+        {
+            player.GetComponent<SkinnedMeshRenderer>().material = blue;
+            ArohaMax++;
+            gameStart = true;
+        }
+        if (!gameStart&&player.transform.parent.transform.parent.transform.position.y <= 30)
+        {
+            player.transform.parent.transform.parent.transform.position = new Vector3(238,90,207);
+        }
+	}
+    public static void ArohaGet()
+    {
+        ArohaCount++;
+    }
+}
